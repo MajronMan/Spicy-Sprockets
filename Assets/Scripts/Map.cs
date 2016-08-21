4e92186fa54beecfcffa442847b6d3cf1ab89aa2
Majron Man <Majron Man>
2016-08-21 23:43:18 +0200

    something to begin working with

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+    public Cell cellPrefab;
+    public Building buildingPrefab;
+    private Cell [,] cells;
+    private Cell highlighted;
+    public IntVector2 size;
+    public Material m3;
+    public void Generate()
+    {
+        cells = new Cell[size.x, size.z];
+        for (int i = 0; i < size.x; i++)
+            for (int j = 0; j < size.z; j++)
+                cells[i, j] = CreateCell(i, j);
+    }
+    private Cell CreateCell(int x, int z)
+    {
+        
+        Cell newCell = Instantiate < Cell >(cellPrefab);
+        if (x % 2 == 0)
+            newCell.m1 = m3;
+        newCell.name = x + ", " + z;
+        newCell.coords = new IntVector2(x, z);
+        newCell.transform.parent = transform;
+        newCell.transform.localPosition = new Vector3(10*x, transform.position.y, 10*z);
+        return newCell;
+    }
+	// Use this for initialization
+	void Start () {
+        Generate();
+	}
+
+    void HighlightCell(RaycastHit hit)
+    {
+        foreach (Cell cell in cells)
+        {
+            if (hit.collider == cell.GetComponentInChildren<MeshCollider>())
+            {
+                if(highlighted != null) 
+                    highlighted.SwitchHighlight(false);
+                highlighted = cell;
+                highlighted.SwitchHighlight();
+                return;
+            }
+        }
+    }
+
+    void StopHighlighting()
+    {
+        if(highlighted != null)
+        {
+            highlighted.SwitchHighlight(false);
+            highlighted = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool p = Physics.Raycast(ray, out hit);
+        if (!p)
+        {
+            StopHighlighting();
+            return;
+        }
+
+        if (highlighted == null || hit.collider != highlighted.col)
+            HighlightCell(hit);
+        if (Input.GetMouseButtonDown(0))
+            OnMouseClick();
+    }
+    private void Build()
+    {
+        Building building = Instantiate < Building >(buildingPrefab);
+        building.GetComponentInChildren<TextMesh>().text = highlighted.coords.x + "," + highlighted.coords.z;
+        building.transform.parent = highlighted.transform;
+        building.transform.localPosition = new Vector3(0,2.5f,0);
+        highlighted.building = building;
+    }
+    void OnMouseClick()
+    {
+        if (highlighted == null || highlighted.building != null) return;
+        Build();
+    }
+}
---
+++ Assets/Scripts/cameramotion.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameramotion : MonoBehaviour {
+
+    void Update()
+    {
+        float speed = 10f;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 v = transform.right;
+            transform.Translate(-v * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 v = transform.right;
+            transform.Translate(v * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 v = transform.forward;
+            transform.Translate(v * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 v = transform.forward;
+            transform.Translate(-v*speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(speed* Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(-speed * Time.deltaTime, 0 , 0));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(new Vector3(0, 0, -speed * Time.deltaTime));
+        }
+
+    }
+}
