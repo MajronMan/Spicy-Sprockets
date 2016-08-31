85dc6871778657da0d8b528a85a0ec3fec008c4a
mprzewie <m.przewie@gmail.com>
2016-09-01 00:09:46 +0200

    Created necessary attributes for abstract Building and Resource

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,5 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Building : MonoBehaviour {
+
+public abstract class Building : MonoBehaviour
+{
+    string name;
+    SortedDictionary<Resource, int> cost;
+
+
 }
