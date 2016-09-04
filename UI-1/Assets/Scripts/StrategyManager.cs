f688aff8ca013e1b8e1f48cb479b276df46e5964
AdamRobertsky <robertadamski96@gmail.com>
2016-09-04 22:09:27 +0200

    UI-1 Interface changes

---
+++ UI-1/Assets/Scripts/StrategyManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrategyManager : MonoBehaviour {
+    public Map mapPrefab;
+    private Map mapInstance;
+
+
+    private void Start()
+    {
+        BeginGame();
+    }
+
+    private void BeginGame()
+    {
+        mapInstance = Instantiate(mapPrefab) as Map;
+    }
+
+    private void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(mapInstance.gameObject);
+        BeginGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+    }
+}
