41659a840442a0d6f6d51982d019029b25440694
Majron Man <majron15@gmail.com>
2016-09-07 21:10:21 +0200

    basic vehicle

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum BuildingSize
+{
+    Penis,
+    Small,
+    Medium,
+    Big,
+}
+
+public enum MovementEnvironment
+{
+    Water,
+    Ground,
+    Underground,
+    Air,
+    Ether
+}
+
+public enum Quality
+{
+    Poverty,
+    Burgeois,
+    Lux
+}
