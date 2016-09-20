740d8ce7c02d1339bcad4a72b4224ff0b96d3790
mprzewie <m.przewie@gmail.com>
2016-09-20 19:18:42 +0200

    added the script ill be working on

---
+++ Assets/Scripts/UnbuildableTerrain.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnbuildableTerrain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    gameObject.AddComponent < BoxCollider2D>();
+	   
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
