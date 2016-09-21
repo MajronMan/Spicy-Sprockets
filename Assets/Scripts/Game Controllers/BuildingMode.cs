a2dc7074c37b1c283f38b4d53b2a420c85d5c44c
mprzewie <m.przewie@gmail.com>
2016-09-21 22:11:21 +0200

    BuildingMode implements necessary methods

--- Assets/Scripts/Game Controllers/BuildingMode.cs
+++ Assets/Scripts/Game Controllers/BuildingMode.cs
@@ -3,5 +3,13 @@ using System.Collections;
 
 public class BuildingMode : GameMode {
 
-	
+    public void RightMouseClicked()
+    {
+        
+    }
+
+    public void LeftMouseClicked()
+    {
+        
+    }
 }
