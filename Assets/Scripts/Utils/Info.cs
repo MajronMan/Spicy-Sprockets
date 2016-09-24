820fd87141799cd6671dee445b8bfed4514a24b9
Miron Markowski <majron15@gmail.com>
2016-09-24 18:08:42 +0200

    Some minor tasks done here

--- Assets/Scripts/Utils/Info.cs
+++ Assets/Scripts/Utils/Info.cs
@@ -9,15 +9,26 @@ using System.Xml.Linq;
 public class Info {
 	public ResourceType ResourceTypes;
 	public Dictionary<string, Resource> Resources = new Dictionary<string, Resource>();
+	public Population ThePeople;
+	public Money MyMoney = new Money();
+	public StrategyManager strategyManager;
+	public List<Building> Buildings;
 
 	public Info(){
+		strategyManager = GameObject.Find ("Strategy Manager");
+		//Get path to file with resource type
 		string path = Directory.GetCurrentDirectory()+@"\Assets\Data\ResourceTypes.xml";
 		XDocument doc = XDocument.Load (path);
+		//Constructor loads types from xml
 		ResourceTypes = new ResourceType (doc.Root.Elements ());
+		//data holds information about resource types
 		var data = ResourceTypes.Data;
 		foreach (var key in data.Keys) {
+			//Create resource instances - arguments mean as follows: name, initial amount, quality, info instance
 			Resource res = new Resource (key, Int32.Parse(data [key] ["initial"]), Quality.Lux, this); 
 			Resources.Add (key, res);
 		}
+		Buildings = strategyManager.GetBuildingManager ().Built; 
+		//I hope it passes a reference, so it's always current list of buildings
 	}
 }
