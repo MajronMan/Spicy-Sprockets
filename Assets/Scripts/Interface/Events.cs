a96c696560a22b309ba83ef3ae60b42c026e3567
AdamRobertsky <robertadamski96@gmail.com>
2016-10-04 23:17:49 +0200

    Events script and setting panel active

---
+++ Assets/Scripts/Interface/Events.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Events : MonoBehaviour {
+
+    private int nextevent;
+    public GameObject EventPanel;
+
+    void Start ()
+    {
+        System.Random rnd = new System.Random();
+        nextevent = rnd.Next(60, 120);
+    }
+	
+	void Update ()
+    {
+
+        if ((int)Time.time == nextevent)
+        {
+            System.Random rnd = new System.Random();
+            int period = rnd.Next(60, 120);
+            nextevent = (int)Time.time + period;
+            Event();
+        }
+    }
+    void Event()
+    {
+        EventPanel.SetActive(true);
+    }
+}
--- Assets/Scripts/Interface/Timer.cs
+++ Assets/Scripts/Interface/Timer.cs
@@ -6,13 +6,10 @@ public class Timer : MonoBehaviour {
 
     public Text timerText;
     private float seconds, minutes, hours;
-    private int nextevent;
 
     void Start()
     {
         timerText = GetComponent<Text>() as Text;
-        System.Random rnd = new System.Random();
-        nextevent = rnd.Next(60, 120);
     }
     void Update()
     {
@@ -20,18 +17,6 @@ public class Timer : MonoBehaviour {
         minutes = (int)((Time.time / 60f) % 60);
         seconds = (int)(Time.time % 60f);
 
-        if((int) Time.time == nextevent)
-        {
-            System.Random rnd = new System.Random();
-            int period = rnd.Next(60, 120);
-            nextevent = (int) Time.time + period;
-            Events();
-        }
-
         timerText.text = hours.ToString() + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
-    }
-    void Events()
-    {
-
     }
 }
