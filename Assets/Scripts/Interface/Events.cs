b3822f7226c2578c0465a1b08c23b34b80221046
AdamRobertsky <robertadamski96@gmail.com>
2016-10-05 22:40:02 +0200

    Creating new panels instead of setting them active

--- Assets/Scripts/Interface/Events.cs
+++ Assets/Scripts/Interface/Events.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Events : MonoBehaviour {
 
-    private int nextevent;
-    public GameObject EventPanel;
+    private const int LayerUI = 5;
+    public GameObject canvasObject;
+    //private int nextevent;
+    //public GameObject EventPanel;
 
     void Start ()
     {
-        System.Random rnd = new System.Random();
-        nextevent = rnd.Next(60, 120);
+        //System.Random rnd = new System.Random();
+        //nextevent = rnd.Next(60, 120);
         StartCoroutine("GetEvent");
     }
 
@@ -17,25 +20,47 @@ public class Events : MonoBehaviour {
     {
         while (true)
         {
+            yield return new WaitForSeconds(new System.Random().Next(1, 5));
             Event();
-            yield return new WaitForSeconds(new System.Random().Next(60, 120));
         }
     }
 	
 	void Update ()
     {
 
-        if ((int)Time.time == nextevent)
+       /* if ((int)Time.time == nextevent)
         {
             System.Random rnd = new System.Random();
             int period = rnd.Next(60, 120);
             nextevent = (int)Time.time + period;
             Event();
         }
+        */
     }
     void Event()
     {
-        var eventGameObject = new GameObject("Event", typeof(Sprite));
-        eventGameObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(50, 50, 50));
+        //var eventGameObject = new GameObject("Event", typeof(Sprite));
+        //eventGameObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(650, 280, 50));
+
+        var eventGameObject = new GameObject("Panel");
+        eventGameObject.transform.SetParent(canvasObject.transform);
+        eventGameObject.layer = LayerUI;
+
+        RectTransform trans = eventGameObject.AddComponent<RectTransform>();
+        trans.anchorMin = new Vector2(0, 0);
+        trans.anchorMax = new Vector2(1, 1);
+        trans.anchoredPosition3D = new Vector3(0, 0, 0);
+        trans.anchoredPosition = new Vector2(0, 0);
+        trans.offsetMin = new Vector2(0, 0);
+        trans.offsetMax = new Vector2(0, 0);
+        trans.localPosition = new Vector3(0, 0, 0);
+        trans.sizeDelta = new Vector2(0, 0);
+        trans.localScale = new Vector3(0.4f, 0.4f, 1.0f);
+        eventGameObject.AddComponent<CanvasRenderer>();
+
+        Image i = eventGameObject.AddComponent<Image>();
+        i.color = Color.cyan;
+
+        eventGameObject.GetComponent<Image> ().sprite = Resources.Load("Panel", typeof(Sprite)) as Sprite;
     }
 }
