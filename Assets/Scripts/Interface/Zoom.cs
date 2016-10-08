2c710664c92f8983573e9b362665c4cfa77f579a
AdamRobertsky <robertadamski96@gmail.com>
2016-10-08 18:41:34 +0200

    Added zoom script

---
+++ Assets/Scripts/Interface/Zoom.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zoom : MonoBehaviour {
+
+	void Update () {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            GetComponent<Camera>().fieldOfView--;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            GetComponent<Camera>().fieldOfView++;
+        }
+    }
+}
