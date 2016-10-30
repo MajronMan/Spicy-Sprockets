0ac7ab99a5847943a446043b754e63e89294b8e0
AdamRobertsky <robertadamski96@gmail.com>
2016-10-30 23:25:33 +0100

    Much more shit added than ever

---
+++ Assets/Scripts/Interface/SlideValue.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SlideValue : MonoBehaviour {
+
+    private GameObject Slider;
+    public Text value;
+
+    public void Start()
+    {
+        Slider = GameObject.Find("TradePanel/Slider");
+    }
+
+    public void Slide()
+    {
+        value.text = Slider.GetComponent<Slider>().value.ToString();
+    }
+}
--- Assets/Scripts/Interface/Trade.cs
+++ Assets/Scripts/Interface/Trade.cs
@@ -2,18 +2,25 @@
 using UnityEngine.UI;
 using System.Collections;
 using UnityEditor;
+using Assets.Scripts.Game_Controllers;
 
 public class Trade : MonoBehaviour {
 
     private GameObject UI;
     private GameObject TransactionImage;
     private GameObject ItemImage;
+    private GameObject Slider;
+    private float money;
+    private string Type;
+    private float resource;
 
     public void Start()
     {
         UI = GameObject.Find("UI").gameObject;
         TransactionImage = GameObject.Find("TradePanel/Transaction");
         ItemImage = GameObject.Find("TradePanel/Item");
+        Slider = GameObject.Find("TradePanel/Slider");
+        money = Controllers.CurrentInfo.MyMoney.GetAmount();
     }
 
     public void Sell()
@@ -24,6 +31,33 @@ public class Trade : MonoBehaviour {
         TransactionImage.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Graphics/Interface graphics&textures/Sell.png");
         TransactionImage.GetComponent<Image>().preserveAspect = true;
 
+        if (ItemImage.GetComponent<Image>().sprite == AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Graphics/Interface graphics&textures/Bread.png"))
+        {
+            Type = "food";
+        }
+        if (ItemImage.GetComponent<Image>().sprite == AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Graphics/Interface graphics&textures/Coal.png"))
+        {
+            Type = "coal";
+        }
+        if (ItemImage.GetComponent<Image>().sprite == AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Graphics/Interface graphics&textures/Metal.png"))
+        {
+            Type = "metal";
+        }
+        if (ItemImage.GetComponent<Image>().sprite == AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Graphics/Interface graphics&textures/Mineral.png"))
+        {
+            Type = "mineral";
+        }
+        if (ItemImage.GetComponent<Image>().sprite == AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Graphics/Interface graphics&textures/Stone.png"))
+        {
+            Type = "stone";
+        }
+        if (ItemImage.GetComponent<Image>().sprite == AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Graphics/Interface graphics&textures/Wood.png"))
+        {
+            Type = "wood";
+        }
+
+        resource = Controllers.CurrentInfo[Type].GetQuantity();
+        Slider.GetComponent<Slider>().maxValue = resource;
     }
     public void Buy()
     {
@@ -32,6 +66,31 @@ public class Trade : MonoBehaviour {
 
         TransactionImage.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Graphics/Interface graphics&textures/Buy.png");
         TransactionImage.GetComponent<Image>().preserveAspect = true;
-    }
 
+        Slider.GetComponent<Slider>().maxValue = money;
+    }
+    public void Confirm()
+    {
+        if (TransactionImage.GetComponent<Image>().sprite == AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Graphics/Interface graphics&textures/Buy.png"))
+        {
+            name = "Buy";
+        }
+        if (TransactionImage.GetComponent<Image>().sprite == AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Graphics/Interface graphics&textures/Sell.png"))
+        {
+            name = "Sell";
+        }
+
+        switch (name)
+        {
+            case "Sell":
+                resource -= Slider.GetComponent<Slider>().value;
+                money += Slider.GetComponent<Slider>().value;
+                break;
+            case "Buy":
+                money -= Slider.GetComponent<Slider>().value;
+                resource += Slider.GetComponent<Slider>().value;
+                break;
+        }
+
+    }
 }
