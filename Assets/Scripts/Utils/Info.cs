7921d674f74d79e1d43d78cc52cf19120dac9c6b
Lukasz Nawojowski <luknwpl@gmail.com>
2016-11-25 23:49:12 +0100

    Info refactor

--- Assets/Scripts/Utils/Info.cs
+++ Assets/Scripts/Utils/Info.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Assets.Scripts.Game_Controllers;
-using Assets.Scripts.Resources;
+using Assets.Scripts.Res;
 using UnityEngine;
 
 namespace Assets.Scripts.Utils {
@@ -9,10 +9,10 @@ namespace Assets.Scripts.Utils {
     /// Contains variable data about a single city
     /// </summary>
     public class Info {
-        public Dictionary<string, Resource> Resources = new Dictionary<string, Resource>();
+        public Dictionary<ResourceType, Resource> Resources = new Dictionary<ResourceType, Resource>();
         public Population ThePeople;
         public Money MyMoney = new Money();
-        public int CurrentStorageVolume;
+        public int UsedStorageVolume;
         public CityController MyCity;
 
         //maybe later use given limits from file or depending on sth
@@ -23,26 +23,20 @@ namespace Assets.Scripts.Utils {
             MyCity = cityController;
         }
 
-        public void LoadInitialResources(Dictionary<string, Dictionary<string, string>> resourceTypes) {
-            foreach (var type in resourceTypes.Keys) {
-                var res = new Resource(type, int.Parse(resourceTypes[type]["initial"]));
-                Resources.Add(type, res);
-                CurrentStorageVolume += res.GetVolume();
+        public void LoadInitialResources(List<Resource> initialResources) {
+            foreach (var resource in initialResources) {
+                Resources.Add(resource.Type, resource);
+                UsedStorageVolume += resource.Volume;
             }
-            var gameObject = new GameObject("People", typeof(Population));
+            var gameObject = new GameObject("People");
+            ThePeople = gameObject.AddComponent<Population>();
             gameObject.transform.SetParent(MyCity.transform);
-            ThePeople = gameObject.GetComponent<Population>();
         }
 
-        public Resource this[string key] {
+        public Resource this[ResourceType key] {
             get { return Resources[key]; }
 
             set { Resources[key] = value; }
-        }
-
-        public Resource this[Resource key] {
-            get { return Resources[key.MyType]; }
-            set { Resources[key.MyType] = value; }
         }
 
         public int GetPopulationLimit() {
@@ -51,18 +45,18 @@ namespace Assets.Scripts.Utils {
 
         public bool SufficientResources(List<Resource> costs) {
             //no idea again but I hope it works
-            return costs.Aggregate(true, (current, resource) => current & Resources[resource.MyType] >= resource);
+            return costs.Aggregate(true, (current, resource) => current && Resources[resource.Type] >= resource);
         }
 
         public bool HasEnoughStorageSpace(int load) {
-            return CurrentStorageVolume + load < _maxStorageVolume;
+            return UsedStorageVolume + load < _maxStorageVolume;
         }
 
         public void UseResources(List<Resource> costs) {
-            foreach (var resource in costs) {
-                Resources[resource.MyType] -= resource;
-                CurrentStorageVolume -= resource.GetVolume();
-            }
+            costs.ForEach(resource => {
+                Resources[resource.Type] -= resource;
+                UsedStorageVolume -= resource.Volume;
+            });
         }
 
         public void BuildingCosts(System.Type buildingType) {
