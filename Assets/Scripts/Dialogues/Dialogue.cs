66d3a005b402db5f6b84930e8a59bc037a033492
malens <malens@bitbucket.org>
2016-12-22 13:42:32 +0100

    sd

---
+++ Assets/Scripts/Dialogues/Dialogue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Dialogues;
+using UnityEngine;
+using Assets.Scripts.Interface;
+using Assets.Static;
+
+namespace Assets.Scripts.Dialogues
+{
+    public class Dialogue : MonoBehaviour
+    {
+        
+        //change to character class
+        private string _character;
+        private string[] dOptions;
+        
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void showDialog()
+        {
+            var dialougeGameObject = Instantiate(Prefabs.DialogueBox);
+
+        }
+    }
+
+
+}
+
