d467544978bb00167c3ae65c80333f0d07b58b59
Lukasz Nawojowski <luknwpl@gmail.com>
2016-12-23 18:17:36 +0100

    Basic popup creation No raycasting - colliders don't work :(

---
+++ Assets/Scripts/Interface/InterfaceUtils.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Static;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Assets.Scripts.Interface {
+    class InterfaceUtils {
+        private static GameObject _activePopup;
+
+        public static GameObject ActivePopup {
+            get { return _activePopup; }
+        }
+
+        public static bool IsPopupActive() {
+            return _activePopup != null;
+        }
+
+        public static GameObject CreatePopup(GameObject parent) {
+            DestroyActivePopup();
+
+            _activePopup = Object.Instantiate(Prefabs.Popup/*, parent.transform*/);
+
+            //determine if the popup will fit on the screen
+            Vector3 position = Input.mousePosition;
+            SpriteRenderer renderer = _activePopup.GetComponent<SpriteRenderer>();
+
+            float width = renderer.sprite.textureRect.width;
+            float height = renderer.sprite.textureRect.height;
+
+            if (position.x + width > Screen.width) position.x -= width;
+            if (position.y - height > Screen.height) position.y += height;
+
+            //move popup, so that the proper corner and not the middle is where the mouse was clicked
+            position.x += width / 2;
+            position.y -= height / 2;
+
+            _activePopup.transform.position = position;
+
+            return _activePopup;
+        }
+
+        public static void DestroyActivePopup() {
+            if (ActivePopup != null)
+                Object.Destroy(ActivePopup);
+        }
+    }
+}
--- Assets/Static/Paths.cs
+++ Assets/Static/Paths.cs
@@ -1,13 +1,13 @@
 ﻿using System.IO;
 
-namespace Assets.Static
-{
+namespace Assets.Static {
     /// <summary>
     /// Container with paths to prefabs, so that if one prefab is renamed, we just change its path here instead of searching for usages
     /// </summary>
-    public static class PrefabPaths
-    {
-        public static string Prefabs { get { return @"Prefabs/"; } }
+    public static class PrefabPaths {
+        public static string Prefabs {
+            get { return @"Prefabs/"; }
+        }
 
         private static string _root = @"Root";
         private static string _map = @"Map";
@@ -23,52 +23,88 @@ namespace Assets.Static
         private static string _gridGroupPanel = @"GridGroupPanel";
         private static string _exitButton = @"ExitButton";
         private static string _buildButton = @"BuildButton";
+        private static string _popup = @"Popup";
 
         /// <summary>
         /// Path of base game object with event system and camera
         /// </summary>
-        public static string Root { get { return Prefabs + _root;} }
+        public static string Root {
+            get { return Prefabs + _root; }
+        }
+
         /// <summary>
         /// path of main map prefab
         /// </summary>
-        public static string Map  { get { return Prefabs + _map; } }
+        public static string Map {
+            get { return Prefabs + _map; }
+        }
+
         /// <summary>
         /// Path of the base of UI
         /// </summary>
-        public static string Canvas { get { return Prefabs + _canvas; } }
+        public static string Canvas {
+            get { return Prefabs + _canvas; }
+        }
+
         /// <summary>
         /// Path of generic building prefab
         /// </summary>
-        public static string Building { get { return Prefabs + _building; } }
+        public static string Building {
+            get { return Prefabs + _building; }
+        }
+
         /// <summary>
         /// Path of a basic UI panel
         /// </summary>
-        public static string Panel { get { return Prefabs + _panel;} }
+        public static string Panel {
+            get { return Prefabs + _panel; }
+        }
 
-        public static string CogwheelButton { get { return Prefabs + _cogwheelButton;} }
+        public static string CogwheelButton {
+            get { return Prefabs + _cogwheelButton; }
+        }
 
-        public static string VerticalGroupPanel { get { return Prefabs + _verticalGroupPanel; } }
+        public static string VerticalGroupPanel {
+            get { return Prefabs + _verticalGroupPanel; }
+        }
 
-        public static string HorizontalGroupPanel { get { return Prefabs + _horizontalGroupPanel; } }
+        public static string HorizontalGroupPanel {
+            get { return Prefabs + _horizontalGroupPanel; }
+        }
 
-        public static string NotRotatingText { get { return Prefabs + _notRotatingText; } }
+        public static string NotRotatingText {
+            get { return Prefabs + _notRotatingText; }
+        }
 
-        public static string CasualButton { get { return Prefabs + _casualButton; } }
+        public static string CasualButton {
+            get { return Prefabs + _casualButton; }
+        }
 
-        public static string ResourceIndicator { get { return Prefabs + _resourceIndicator; } }
+        public static string ResourceIndicator {
+            get { return Prefabs + _resourceIndicator; }
+        }
 
-        public static string GridGroupPanel { get { return Prefabs + _gridGroupPanel; } }
+        public static string GridGroupPanel {
+            get { return Prefabs + _gridGroupPanel; }
+        }
 
-        public static string ExitButton { get { return Prefabs + _exitButton; } }
+        public static string ExitButton {
+            get { return Prefabs + _exitButton; }
+        }
 
-        public static string BuildButton { get {return Prefabs + _buildButton;} }
+        public static string BuildButton {
+            get { return Prefabs + _buildButton; }
+        }
+
+        public static string Popup {
+            get { return Prefabs + _popup; }
+        }
     }
 
     /// <summary>
     /// Container with paths to sprites, so that if one sprite is renamed, we just change its path here instead of searching for usages
     /// </summary>
-    public static class GraphicsPaths
-    {
+    public static class GraphicsPaths {
         public static string Graphics = @"Graphics/";
         public static string InterfaceGraphics = Graphics + @"Interface/";
         public static string ResourcePoolGraphics = Graphics + @"Pools/";
@@ -77,15 +113,32 @@ namespace Assets.Static
         private static string _coal = @"Coal";
         private static string _metal = @"Metal";
         private static string _wood = @"Wood";
-        private static string _stone= @"Stone";
+        private static string _stone = @"Stone";
         private static string _mineral = @"Mineral";
 
-        public static string Food { get { return InterfaceGraphics + _food; } }
-        public static string Coal { get { return InterfaceGraphics + _coal; } }
-        public static string Metal { get { return InterfaceGraphics + _metal; } }
-        public static string Wood { get { return InterfaceGraphics + _wood; } }
-        public static string Stone { get { return InterfaceGraphics + _stone; } }
-        public static string Mineral { get { return InterfaceGraphics + _mineral; } }
+        public static string Food {
+            get { return InterfaceGraphics + _food; }
+        }
+
+        public static string Coal {
+            get { return InterfaceGraphics + _coal; }
+        }
+
+        public static string Metal {
+            get { return InterfaceGraphics + _metal; }
+        }
+
+        public static string Wood {
+            get { return InterfaceGraphics + _wood; }
+        }
+
+        public static string Stone {
+            get { return InterfaceGraphics + _stone; }
+        }
+
+        public static string Mineral {
+            get { return InterfaceGraphics + _mineral; }
+        }
 
         public static string[] ResourcesSprites = {Food, Coal, Metal, Wood, Stone, Mineral};
     }
