fc7f54d09e14c8e0488dfa18f62db9f61c84161f
Lukasz Nawojowski <luknwpl@gmail.com>
2017-02-03 03:17:09 +0100

    Added IResourceStorage

---
+++ Assets/Scripts/Buildings/IResourceStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Res;
+
+namespace Assets.Scripts.Buildings {
+    /// <summary>
+    /// Represents the ability to store resources
+    /// </summary>
+    interface IResourceStorage {
+        Dictionary<ResourceType, Resource> Stored { get; }
+    }
+}
