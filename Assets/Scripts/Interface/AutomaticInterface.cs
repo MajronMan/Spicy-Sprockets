e2861427fd5ce5e019d702f10251fd0175940eef
Lukasz Nawojowski <luknwpl@gmail.com>
2017-02-17 19:34:59 +0100

    Add toggle grid button

--- Assets/Scripts/Interface/AutomaticInterface.cs
+++ Assets/Scripts/Interface/AutomaticInterface.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Assets.Scripts.Game_Controllers;
 using Assets.Scripts.Res;
 using Assets.Scripts.Utils;
 using Assets.Static;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace Assets.Scripts.Interface
-{
-    public class AutomaticInterface : MonoBehaviour
-    {
+namespace Assets.Scripts.Interface {
+    public class AutomaticInterface : MonoBehaviour {
         /// <summary>
         /// The panel which allows performing most of actions in game
         /// </summary>
         public GameObject MainPanel;
+
         private Rect _mainPanelRect = new Rect(0.9f, 0.3f, 0.1f, 0.7f);
+
         /// <summary>
         /// Panel with amounts of resources in current city
         /// </summary>
         public GameObject ResourcePanel;
+
         private Rect _resourcePanelRect = new Rect(0.1f, 0, 0.75f, 0.05f);
+
         /// <summary>
         /// Panel which shows minimap
         /// </summary>
         public GameObject MiniMapPanel;
+
         private Rect _miniMapPanelRect = new Rect(0.9f, 0, 0.1f, 0.25f);
 
         private Rect _centerRect = new Rect(0.2f, 0.1f, 0.6f, 0.8f);
@@ -38,30 +38,51 @@ namespace Assets.Scripts.Interface
 
         private Dictionary<string, ExitablePanel> _buttonPanels;
 
-        public void Start ()
-        {
+        public void Start() {
             CreatePanels();
             CreateButtons();
         }
 
-       
-        private void CreatePanels()
-        {
+
+        private void CreatePanels() {
             MainPanel = Instantiate(Prefabs.VerticalGroupPanel);
             Util.SetUIObjectPosition(MainPanel, _mainPanelRect, transform);
 
-            MiniMapPanel = Instantiate(Prefabs.Panel);
-            Util.SetUIObjectPosition(MiniMapPanel, _miniMapPanelRect, transform);
-            
+            CreateMiniMapPanel();
+
             CreateResourcePanel();
 
             PeopleAndMoney();
         }
 
-        private void CreateButtons()
-        {
-            _buttonPanels = new Dictionary<string, ExitablePanel>
-            {
+        private void CreateMiniMapPanel() {
+            MiniMapPanel = Instantiate(Prefabs.Panel);
+            Util.SetUIObjectPosition(MiniMapPanel, _miniMapPanelRect, transform);
+
+            CreateToggleGridButton();
+        }
+
+        private void CreateToggleGridButton() {
+            var gridToggleButton = Instantiate(Prefabs.TextButton);
+
+            Util.SetUIObjectPosition(gridToggleButton, new Rect(0.0f, 0.75f, 1.0f, 0.25f), MiniMapPanel.transform);
+
+            gridToggleButton.name = "GridToggleButton";
+
+            var text = gridToggleButton.GetComponentInChildren<Text>();
+            if (text == null) Debug.Log("Text component not found in the hierarchy of TextButton prefab");
+
+            text.text = "Toggle grid";
+            text.resizeTextMaxSize = 12;
+
+            var button = gridToggleButton.GetComponentInChildren<Button>();
+            if (button == null) Debug.Log("Button component not found in the hierarchy of TextButton prefab");
+
+            button.onClick.AddListener(() => { Controllers.CurrentCityController.MapInstance.ToggleGrid(); });
+        }
+
+        private void CreateButtons() {
+            _buttonPanels = new Dictionary<string, ExitablePanel> {
                 {"Production", CreateExitablePanel(Prefabs.Panel)},
                 {"Character", CreateExitablePanel(Prefabs.Panel)},
                 {"Diplomacy", CreateExitablePanel(Prefabs.Panel)},
@@ -70,8 +91,7 @@ namespace Assets.Scripts.Interface
                 {"Build", CreateExitablePanel(Prefabs.GridGroupPanel)},
                 {"Trade", CreateExitablePanel(Prefabs.Panel)}
             };
-            foreach (var namesToPanels in _buttonPanels)
-            {
+            foreach (var namesToPanels in _buttonPanels) {
                 var panel = namesToPanels.Value;
                 panel.name = namesToPanels.Key + "Panel";
                 Util.SetUIObjectPosition(panel.gameObject, _centerRect, transform);
@@ -80,10 +100,8 @@ namespace Assets.Scripts.Interface
                 buttonGameObject.name = namesToPanels.Key + "Button";
                 buttonGameObject.transform.SetParent(MainPanel.transform);
                 AddNotRotatingTextToButton(buttonGameObject, namesToPanels.Key);
-                buttonGameObject.GetComponent<Button>().onClick.AddListener(() =>
-                {
-                    foreach (var pair in _buttonPanels)
-                    {
+                buttonGameObject.GetComponent<Button>().onClick.AddListener(() => {
+                    foreach (var pair in _buttonPanels) {
                         pair.Value.gameObject.SetActive(false);
                     }
                     panel.gameObject.SetActive(true);
@@ -99,8 +117,7 @@ namespace Assets.Scripts.Interface
             //globalMapButton.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene(""));
         }
 
-        private ExitablePanel CreateExitablePanel(GameObject child)
-        {
+        private ExitablePanel CreateExitablePanel(GameObject child) {
             var panel = Instantiate(Prefabs.ExitablePanel);
             var inner = Instantiate(child);
             Util.SetUIObjectPosition(inner, _fullRect, panel.transform);
@@ -109,8 +126,7 @@ namespace Assets.Scripts.Interface
             return exitable;
         }
 
-        private void AddNotRotatingTextToButton(GameObject button, string text)
-        {
+        private void AddNotRotatingTextToButton(GameObject button, string text) {
             var buttonRectTransform = button.GetComponent<RectTransform>();
             var textGO = Instantiate(Prefabs.NotRotatingText);
 
@@ -123,13 +139,11 @@ namespace Assets.Scripts.Interface
             textRectTransform.offsetMin = -buttonRectTransform.sizeDelta;
         }
 
-        private void CreateResourcePanel()
-        {
+        private void CreateResourcePanel() {
             ResourcePanel = Instantiate(Prefabs.HorizontalGroupPanel);
             Util.SetUIObjectPosition(ResourcePanel, _resourcePanelRect, transform);
 
-            foreach (var type in Controllers.ConstantData.ResourceTypes)
-            {
+            foreach (var type in Controllers.ConstantData.ResourceTypes) {
                 var indicator = Instantiate(Prefabs.ResourceIndicator);
                 indicator.transform.SetParent(ResourcePanel.transform);
                 indicator.GetComponentInChildren<Image>().sprite = Sprites.ResourceSprite(type);
@@ -140,28 +154,24 @@ namespace Assets.Scripts.Interface
         }
 
 
-        public static Rect CenterOfScreenRect(float width, float height)
-        {
-            var relativeWidth = width/Screen.width;
-            var relativeHeight = height/Screen.height;
-            var posx = (Screen.width - width)/(2.0f * Screen.width);
-            var posy = (Screen.height - height)/(2.0f * Screen.height);
+        public static Rect CenterOfScreenRect(float width, float height) {
+            var relativeWidth = width / Screen.width;
+            var relativeHeight = height / Screen.height;
+            var posx = (Screen.width - width) / (2.0f * Screen.width);
+            var posy = (Screen.height - height) / (2.0f * Screen.height);
             return new Rect(posx, posy, relativeWidth, relativeHeight);
         }
 
-        private void FillBuildingsPanel()
-        {
+        private void FillBuildingsPanel() {
             var buildingPanel = _buttonPanels["Build"];
-            foreach (var building in Controllers.ConstantData.BuildingCosts.Keys)
-            {
+            foreach (var building in Controllers.ConstantData.BuildingCosts.Keys) {
                 var button = Instantiate(Prefabs.BuildButton);
                 button.transform.SetParent(buildingPanel.Content.transform);
                 button.GetComponent<BuildButton>().SetUp(building, buildingPanel.gameObject);
             }
         }
 
-        private void PeopleAndMoney()
-        {
+        private void PeopleAndMoney() {
             var panel = Instantiate(Prefabs.VerticalGroupPanel);
             Util.SetUIObjectPosition(panel, _peopleMoneyRect, transform);
 
